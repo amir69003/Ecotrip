d04bba6882cb8d068462468ddb4b57f6d1026ca4
Munoz Angel <angel.munoz@etu.univ-lyon1.fr>
2025-05-14 19:18:22 +0200

    Tests qui fonctionnent pour le userController localement et sur la vm

--- frontend/tests/config.js
+++ frontend/tests/config.js
@@ -1,4 +1,7 @@
 const pathLocal = "http://localhost:8080/";
 const path = "http://192.168.75.123/";
 
-export { pathLocal, path };
+const idLocal = 11;
+const id = 3;
+
+export { pathLocal, path, idLocal, id };
