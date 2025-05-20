4a46ef77dfeea53cfe9be2695d99f7dec75be074
Munoz Angel <angel.munoz@etu.univ-lyon1.fr>
2025-05-20 15:19:45 +0200

    prise

--- frontend/tests/config.js
+++ frontend/tests/config.js
@@ -2,6 +2,6 @@ const pathLocal = "http://localhost:8080/";
 const path = "http://192.168.75.123/";
 
 const idLocal = 11;
-const idVm = 1;
+const idVm = 3;
 
 export { pathLocal, path, idLocal, idVm };
